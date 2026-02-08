973644bc217a2fce2c897b1bc54674ce7431c3d5
Niels Hartog <niels@fijnbesnaard.nl>
2026-02-08 19:46:27 +0100

    Reset repository to start

--- src/app/keystatic/[[...conf]]/page.tsx
+++
@@ -1,9 +0,0 @@
-import { makePage } from "@keystatic/next/ui/app";
-import config from "../../../../keystatic.config";
-
-export const dynamic = "force-static";
-export function generateStaticParams() {
-  return [{ conf: [] }];
-}
-
-export default makePage(config);
--- src/lib/markdoc.ts
+++
@@ -1,8 +0,0 @@
-import Markdoc from "@markdoc/markdoc";
-import React from "react";
-
-export function renderMarkdoc(content: any) {
-  const ast = Markdoc.parse(content);
-  const transformed = Markdoc.transform(ast);
-  return Markdoc.renderers.react(transformed, React);
-}
